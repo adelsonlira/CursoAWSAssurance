c1a8cbbbcc5df5f3410c2ecb9e8a6a873537d2df
Adelson Lira <adelsonlira@yahoo.com.br>
2026-06-09 21:00:57 -0300

    retorno status ok json

--- src/main/java/com/cursoaws/assurance/teste/helth.java
+++ src/main/java/com/cursoaws/assurance/teste/helth.java
@@ -1,12 +1,15 @@
 package com.cursoaws.assurance.teste;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Map;
+
 @RestController
 public class helth {
-    @GetMapping("/helth")
-    public String helth() {
-        return "ok";
+    @GetMapping("/health")
+    public ResponseEntity<Map<String, String>> health() {
+        return ResponseEntity.ok(Map.of("status", "ok"));
     }
 }
